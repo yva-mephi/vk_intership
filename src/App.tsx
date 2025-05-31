75b1e25adb7d2e482cba91325c992d0ce84d7c41
yva-mephi <yva011@campus.mephi.ru>
2025-05-31 21:09:49 +0300

    feat: :tada: Start project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MainPage } from "./pages/mainPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
