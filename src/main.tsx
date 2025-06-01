451d86251e2aa6d60658154ae5ad08634a3147fe
yva-mephi <yva011@campus.mephi.ru>
2025-06-01 14:49:17 +0300

    style: :lipstick: add MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0077FF", // vk-акцент
+    },
+    secondary: {
+      main: "#3B82F6",
+    },
+    background: {
+      default: "#FFFFFF", // белый фон
+      paper: "#f9f9f9", // для карточек и контейнеров
+    },
+    text: {
+      primary: "#000000", // чёрный текст
+      secondary: "#555555", // серый для второстепенного текста
+    },
+    action: {
+      active: "#4F9CFF", // для активных элементов
+    },
+  },
+  typography: {
+    fontFamily: ['"Inter"', "sans-serif"].join(","),
+    h4: {
+      fontWeight: 600,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
